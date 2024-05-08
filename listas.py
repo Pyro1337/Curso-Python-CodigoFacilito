64c455348b5baa956ee9428d6786a7f216016c67
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 16:32:56 -0400

    Create listas.py
    La forma de declarar todas las listas posibles, si se declara una lista que contiene varios tipos de datos entonces es una lista general nada mas, a no ser que se declaren listas de un mismo tipo.

---
+++ listas.py
@@ -0,0 +1,11 @@
+#Para creacion de listas podemos usamos list() o mediante corchetes de la siguiente forma:
+lista = ['String', 10 , 10.5 , True] #Este list puede contener todo tipo de dato y quedaria tal que asi
+print (lista)
+#A cada valor de la lista, le corresponde un indice, el String le corresponde 0
+lista_strings = ["Ivan" , "Curso", "Python"]
+
+lista_enteros = [-10, 10 ,0 , 300]
+
+lista_floats = [10.5 , -20.5 , 30.5 , 100.00]
+
+lista_booleanos = [True , True, False , (2>10), (2<=10), (4 > 3 and 10 != 11)]
