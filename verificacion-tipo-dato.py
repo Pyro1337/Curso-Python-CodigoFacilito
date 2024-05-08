3bfc79daa8e56c4a887cee65d8f9584b557f822e
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 14:25:31 -0400

    Create verificacion-tipo-dato.py
    Palabra reservada type que nos permite verificar que tipo de dato estamos tratando , basta con usar type(variable)

---
+++ verificacion-tipo-dato.py
@@ -0,0 +1,11 @@
+#En python para verificar que tipo de dato es una variable basta con pasarle la palabra reservada type tal que :
+#type(variable);
+#este nos retornara que tipo de dato es, por ejemplo : 
+cadena_prueba = 'Hola mundo en cadena';
+print(type(cadena_prueba));
+int_prueba = 33;
+print(type(int_prueba));
+bool_prueba = True;
+print(type(bool_prueba));
+float_prueba = 3.14;
+print(type(float_prueba));
