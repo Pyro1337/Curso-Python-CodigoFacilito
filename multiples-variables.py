9dac56dac6a78cdda3f281ddbb885b0ac19334d2
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 15:10:03 -0400

    Create multiples-variables.py
    La asignación de múltiples variables en una linea de código facilita la creación y manipulación de los mismos ahorrando líneas de código y formando buenas prácticas

---
+++ multiples-variables.py
@@ -0,0 +1,11 @@
+nombre = 'Ivan'
+apellido = 'Sanchez'
+titulo = 'Lic. '
+print(nombre)
+print(apellido)
+print(titulo)
+
+#Una forma de declarar multiples variables de una y entre comas es la siguiente 
+
+nom, ape, tit = 'Ivan' , 'Sanchez' , 'Lic. '
+print(nom,"\n",ape,"\n",tit)
