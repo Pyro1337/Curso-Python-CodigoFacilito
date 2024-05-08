89c435ea9a9df2b344016695dc08a370d9605d9a
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 16:42:54 -0400

    Update listas.py
    Agregamos la búsqueda de posiciones en lista mediante [posiciones]

--- listas.py
+++ listas.py
@@ -9,3 +9,10 @@ lista_enteros = [-10, 10 ,0 , 300]
 lista_floats = [10.5 , -20.5 , 30.5 , 100.00]
 
 lista_booleanos = [True , True, False , (2>10), (2<=10), (4 > 3 and 10 != 11)]
+#                0           1        2       3       4
+lista_cursos = ['Python', 'Django', 'Flask', 'Java', 'Ruby']
+#                1          2         3        4       5
+primer_curso = lista_cursos[3]
+print("El curso en la 4ta posicion es : ",primer_curso)
+
+#si se realizan busquedas en una lista con una posicion que sobrepasa la cantidad entonces saca un out of range.
