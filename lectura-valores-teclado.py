4bcf3cd4d21cd29c82b7af1cde348b23164727a0
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 14:31:43 -0400

    Create lectura-valores-teclado.py
    Para introducir por teclado variables, basta con utilizar la palabra reservada input, tal que variable = input("Mensaje de contexto de entrada de datos");

---
+++ lectura-valores-teclado.py
@@ -0,0 +1,7 @@
+#Para obtener valores por teclado usaremos la funcion input("Mensaje de contexto de ingreso: ");
+#input("Ingresa tu nombre completo: ");
+#para almacenarlo en una variable basta con variable = input ("Mensaje de contexto de ingreso ");
+nombreCompleto = input("Ingrese el nombre completo: ");
+print("El nombre completo es : ",nombreCompleto);
+#Tener en cuenta que input siempre retorna un tipo de dato STR esto se confirma si se hace lo siguiente : 
+print(type(nombreCompleto));
