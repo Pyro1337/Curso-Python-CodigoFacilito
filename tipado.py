63044d97b027dd5755e22828ee4fbaf792d58cce
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 14:01:33 -0400

    Create tipado.py
    Esta clase nos indica que Python es un lenguaje del tipo de tipado dinámico, es decir a medida que se avanza con el programa una variable puede tomar distinto tipos de datos no solamente int, sino que a la hora de imprimir tomara el ultimo valor que se le haya asignado por más que este haya iniciado siendo un int , si al final se le asigna un true tomará como una variable tipo booleana.

---
+++ tipado.py
@@ -0,0 +1,7 @@
+#Clase 10 Tipados
+"""Esto nos indica que el tipo de tipado es dinamico es decir que puede asignarse distintos tipos de datos
+y siempre tomara el ultimo asignado"""
+valor = 'Ivan';
+valor = 2;
+valor = True;
+print (valor);
