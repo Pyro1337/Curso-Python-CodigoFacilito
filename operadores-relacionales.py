ef66981e00a1f96f31f459ea38af1dde756fa6eb
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 14:08:56 -0400

    Create operadores-relacionales.py
    Esta clase nos indica los operadores relacionales mas comunes que existe en Python entre ellas :
    > , < , <= , >= , != , ==

---
+++ operadores-relacionales.py
@@ -0,0 +1,23 @@
+numero_uno = 10;
+numero_dos = 20;
+
+"""Estos operadores relacionales son:
+> Mayor
+< menor
+<= menor o igual
+>= Mayor o igual
+== igual
+!= distinto"""
+print("Numero uno : ",numero_uno,"\nNumero dos : ",numero_dos);
+resultado = numero_uno > numero_dos;
+print("Numero uno es mayor que numero dos ?: ",resultado);
+resultado = numero_uno < numero_dos;
+print("Numero uno es menor que numero dos ?: ",resultado);
+resultado = numero_uno <= numero_dos;
+print("Numero uno es menor o igual a numero dos ?: ",resultado);
+resultado = numero_uno >= numero_dos;
+print("Numero uno es menor o igual a numero dos ?: ",resultado);
+resultado = numero_uno == numero_dos;
+print("Numero uno es igual a numero dos ? : ",resultado);
+resultado = numero_uno != numero_dos;
+print("Numero uno es distinto a numero dos ? : ",resultado);
