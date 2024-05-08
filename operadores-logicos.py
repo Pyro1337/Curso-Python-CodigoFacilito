9023e3ec745ad2ddbab8997eff2b5dc7dece6f0c
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 14:13:31 -0400

    Create operadores-logicos.py
    En esta clase indagamos sobre como actúan los operadores logicos de negación, conjunción y disyunción

---
+++ operadores-logicos.py
@@ -0,0 +1,14 @@
+#Entre los operadores logicos nos encontramos a and, or y se comparan valores booleanos para obtener valores booleanos.
+#Para and todas las comparaciones deben ser verdaderas.
+#Para or basta que con una sea verdadera
+#Para not no debe ser, es decir es lo opuesto.
+resultado_final = True and True;
+print(resultado_final);
+resultado_final = True and False;
+print(resultado_final);
+resultado_final = True or False;
+print(resultado_final);
+resultado_final = not True;
+print(resultado_final);
+resultado_final = not False;
+print(resultado_final);
