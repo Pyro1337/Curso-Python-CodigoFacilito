c4e5b1c8a5e151425ac9d9ab135ef6be3e8ae276
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-09 15:04:52 -0400

    Otras formas de tener valores falsos

--- none.py
+++ none.py
@@ -4,3 +4,17 @@ print(resultado)
 print(type(resultado))
 resultado = 5
 print("Una vez cargado nos da : ",resultado)
+
+#valores falsos para representar falsos son 
+#True (1) False (0)
+"""
+False
+None
+0
+0.0
+'' / ""
+[]
+{}
+()
+Todos estos pueden tomarse como falsos no unicamente el None o False
+"""
