0be83376d1742f6d473a0417327d13aa15a4dc11
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-09 09:49:32 -0400

    se asignan las tuplas, mediante valores en posiciones , concepto de desempaquetado de tuplas

---
+++ desempaquetado.py
@@ -0,0 +1,10 @@
+#Podemos crear una lista que a su vez se le asigne valores por ejemplo creamos una lista de numeros y le asignamos
+#su numero en letras a cada uno segun la posicion que ocupe.
+anlex = (1,2,3,4,5)
+uno , dos , tres, cuatro ,cinco = anlex
+
+print(uno)
+print(dos)
+print(tres)
+print(cuatro)
+print(cinco)
