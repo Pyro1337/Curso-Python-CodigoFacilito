db7e5f8569224e17642a241c9b515544f9f32864
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-09 15:02:53 -0400

    uso del none para valores desconocidos

---
+++ none.py
@@ -0,0 +1,6 @@
+#Cuando no sabemos o no tenemos aun el valor de alguna de las variables podemos cargarlas con none
+resultado = None #con la N en mayuscula
+print(resultado)
+print(type(resultado))
+resultado = 5
+print("Una vez cargado nos da : ",resultado)
