25f26a14ae82c23b065e4eae053fbd1b9ea18a09
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-09 15:11:12 -0400

    Condiciones mediante if, elif

---
+++ condiciones-multiples.py
@@ -0,0 +1,8 @@
+#Para evaluar multiples condiciones mediante un elif
+calificacion = 8
+if calificacion == 10 :
+    print("Aprobo la materia con la mayor calificacion")
+elif calificacion >= 5 and calificacion <=9:
+    print("Aprobaste la materia")
+elif calificacion < 5:
+    print("Reprobaste la materia")
