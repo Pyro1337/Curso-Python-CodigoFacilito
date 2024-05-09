0bb872f4e43ca6b5165a727581e0e5fb2ddccd5e
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-08 21:17:58 -0400

    Se trato sobre los metodos principales para modificar listas, agregar, reemplazar, eliminar por posicion por contenido y limpiar la lista, acoplar y demas

--- listas.py
+++ listas.py
@@ -35,12 +35,41 @@ print(sub_lista) #Se imprime Python, Django Flask
 #[start:end:skip] -> El skip serian los SALTOS que hara.
 
 
-#como agregar elementos a una lista.
+#Agregar elementos a la lista
 lista_cursos.append('MongoDB') #Para agregar un elemento a la lista basta con utilizar append.
 print(lista_cursos)
 
-#Reemplazar el valor y mover la lista actualmente tenemos hasta :
+#Reemplazar el valor y mover la lista una posicion hacia la derechaa actualmente tenemos hasta :
 #['Python', 'Django', 'Flask', 'Java', 'Rust', 'MongoDB'] entonces si realizo un lista_cursos.insert(0,'Pygame')
 #entonces colocaria a Python en la posicion 1 y la 0 corresponderia ahora a Pygame
 lista_cursos.insert(0,'Pygame')
 print(lista_cursos) #nos devuelve ['Pygame', 'Python', 'Django', 'Flask', 'Java', 'Rust', 'MongoDB']
+lista_cursos.insert(1,'PHP')
+print(lista_cursos) # esto nos debe devolver ['Pygame', ,'PHP','Python', 'Django', 'Flask', 'Java', 'Rust', 'MongoDB']
+
+#Longitud de una lista len(nombre_lista)
+print("La longitud de la lista es de: ",len(lista_cursos)," cursos.")
+
+#Para acoplar una lista pequeña a otra mas grande procederemos a crear un lista_cursos_2 agregaremos  algunos datos
+#y posteriormente utilizaremos  la funcion extend para acoplar esa lista  mas chica a la principal desde su final.
+lista_cursos_2 = ['C','C++','C#','Docker','GIT']
+#Procedemos a acoplar la segunda lista a la primera : lista1.extend(lista2)
+lista_cursos.extend(lista_cursos_2)
+print("La lista acoplada es : ",lista_cursos) #nos devuelve ['Pygame', 'PHP', 'Python', 'Django', 'Flask', 'Java', 'Rust', 'MongoDB', 'C', 'C++', 'C#', 'Docker', 'GIT']
+
+#Eliminar elementos de la lista por su contenido
+#Supongamos que queremos eliminar Docker de la lista_cursos_2 utilizamos el metodo .remove('NombreElemento')
+lista_cursos_2.remove('Docker')
+print("La lista luego de eliminar Docker es : ",lista_cursos_2)
+
+#Eliminar elementos de la lista por su posicion
+#En este caso utilizaremos la  palabra reservada del nombre_lista[posicion]
+#Supongamos que actualmente tenemos en lista_cursos_2 : ['C', 'C++', 'C#', 'GIT']
+#Eliminemos la posicion 2 que corresponde a C#
+del lista_cursos_2[2]
+print("El resultado de eliminar la segunda posicion de la lista_cursos_2 es : ",lista_cursos_2)
+
+#Eliminar todos los elementos de la lista
+#Utilizamos el metodo clear lista.clear()
+lista_cursos_2.clear()
+print("La lista cursos 2 limpia es : ",lista_cursos_2," cuya longitud es ",len(lista_cursos_2))
