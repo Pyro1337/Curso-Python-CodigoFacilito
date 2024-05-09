0632440cd2a5a1c65dbbdef731c44ec261134c7b
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-05-09 15:06:56 -0400

    Condiciones en Python mediante IF

---
+++ condiciones.py
@@ -0,0 +1,14 @@
+#Para poder realizar condiciones utilizaremos if
+#Todos los bloques if se identan con 4 espacios por convencion de desarrolladores Python.
+resultado = 10
+if resultado >= 10 :
+    print("El resultado es mayor o igual a 10") 
+else: 
+    print("No cumple la condicion")
+
+if resultado > 0 and resultado <=10 : 
+    print("Cumple la condicion de estar entre 0 y 10")
+else:
+    print("No cumple la condicion")
+
+
