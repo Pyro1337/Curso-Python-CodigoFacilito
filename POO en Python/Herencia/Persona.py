33b7284d55de7d2a5555f6079ede5669285e743a
Ivan Sanchez <136512762+Pyro1337@users.noreply.github.com>
2024-08-09 15:09:15 -0400

    Se coloca el ejemplo de la clase hija Desarollador que hereda propiedades de Persona y ademas contiene atributos propios.

--- POO en Python/Herencia/Persona.py
+++ POO en Python/Herencia/Persona.py
@@ -1,21 +1,49 @@
 class Persona:
-    def __init__(self,nombre,edad,nacionalidad):
+    def __init__(self, nombre, edad, nacionalidad):
         self.nombre = nombre
         self.edad = edad
         self.nacionalidad = nacionalidad
 
     def hablar(self):
-        print("La persona esta hablando")
+        print("La persona está hablando")
 
-class Empleado(Persona): #-> De esta manera decimos que empleado es hijo de la clase padre Persona
-    #Empleado tomara los mismos atributos de Persona pues lo heredamos.
+class Empleado(Persona): 
     def trabajar(self):
-        print("El empleado esta trabajando")
-    
+        print("El empleado está trabajando")
 
-#Se asignan los valores para E1 -> Correspondiente a Empleado
-E1 = Empleado("Ivan",27,"paraguayo")
-print(E1.nombre+"\n"+str(E1.edad)+"\n"+E1.nacionalidad)
-#Se llaman a las funciones, una es de la clase padre Personas la otra de la clase hija Empleado
+# Se asignan los valores para E1 -> Correspondiente a Empleado
+E1 = Empleado("Ivan", 27, "paraguayo")
+print(E1.nombre + "\n" + str(E1.edad) + "\n" + E1.nacionalidad)
+# Se llaman a las funciones, una es de la clase padre Persona, la otra de la clase hija Empleado
 E1.hablar()
-E1.trabajar()    
+E1.trabajar()
+
+# Herencia entre Persona y Desarrollador, con nuevos atributos para Desarrollador
+class Desarrollador(Persona):
+    def __init__(self, nombre, edad, nacionalidad, salario, nivel, lenguajes):
+        super().__init__(nombre, edad, nacionalidad)
+        self.salario = salario
+        self.nivel = nivel
+        self.lenguajes = lenguajes
+
+    def programacion(self):
+        print(f"El dev {self.nombre}, cuyo nivel es {self.nivel}, se encuentra programando en el lenguaje {self.lenguajes}")
+            
+
+# Main
+nombre = input("Ingrese el nombre del desarrollador: ")
+edad = int(input("Ingrese la edad del Dev: "))
+salario = float(input("Ingrese el salario en $: "))
+nacionalidad = input("Ingrese la nacionalidad del Dev: ")
+nivel = input("¿Qué nivel de desarrollo posee? : ")
+lenguajes = []  # Creamos una lista vacía primeramente
+
+while True:
+    lenguaje = input("Ingrese el lenguaje que maneja, una vez finalizado presione X: ")
+    if lenguaje.lower() == 'x':
+        break
+    lenguajes.append(lenguaje)
+
+dev1 = Desarrollador(nombre, edad, nacionalidad, salario, nivel, lenguajes)
+dev1.programacion()
+dev1.hablar()
